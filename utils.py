b018070abf5efac8e8fbd5872f40c2ad7709219f
Lokesh Gupta <124081689+Lokesh-2310@users.noreply.github.com>
2025-06-18 16:26:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
-GROQ_API_KEY="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"
+import os
+# secret_value = os.environ.get("YOUR_SECRET_NAME")
+GROQ_API_KEY=os.environ.get("YOUR_SECRET_NAME")
 VOICE_TO_TEXT_MODEL="whisper-large-v3"
 CHAT_MODEL="llama-3.3-70b-versatile"
