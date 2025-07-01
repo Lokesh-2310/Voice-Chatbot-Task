21be2d8030139f885b417042f703589042beec8b
Lokesh Gupta <124081689+Lokesh-2310@users.noreply.github.com>
2025-07-02 00:17:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 import streamlit as st
-GROQ_API_KEY=st.secrets["GROQ_API_KEY"]
+GROQ_API_KEY="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"
 VOICE_TO_TEXT_MODEL="whisper-large-v3"
 CHAT_MODEL="llama-3.3-70b-versatile"
